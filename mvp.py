7512ec6999202cd6628a1e172980ff0ee48204df
Арсений Казанцев <kazars24@MacBook-Air-Arsenij.local>
2023-07-06 21:44:10 +0300

    update plots

--- mvp.py
+++ mvp.py
@@ -2,13 +2,12 @@ import time
 
 import av
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import plotly.express as px
-import seaborn as sns
 import streamlit as st
 from PIL import Image
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
 from utils.model_class import EmoRecNet
 
@@ -22,14 +21,27 @@ EMOTION_COUNT = {'angry': 0,
                  'surprise': 0,
                  'neutral': 0}
 LINE_DATA = []
-model = EmoRecNet()
+if 'emotion_count_dict' not in st.session_state:
+    st.session_state['emotion_count_dict'] = {'angry': 0,
+                                              'disgust': 0,
+                                              'fear': 0,
+                                              'happy': 0,
+                                              'sad': 0,
+                                              'surprise': 0,
+                                              'neutral': 0}
+if 'emotion_dinamics' not in st.session_state:
+    st.session_state['emotion_dinamics'] = []
+if 'analyze' not in st.session_state:
+    st.session_state['analyze'] = False
 
+model = EmoRecNet()
 
 def callback(frame):
     img = frame.to_ndarray(format="bgr24")
     img = Image.fromarray(img)
 
     captured_emotions = model.predict(img)
+
 
     for face in captured_emotions:
         box = face['bbox']
@@ -61,19 +73,30 @@ def callback(frame):
 
 
 def make_barchart(placeholder):
-    fig = plt.figure(figsize=(10, 5))
-    sns.barplot(x=list(EMOTION_COUNT.keys()), y=list(EMOTION_COUNT.values()))
+    if st.session_state['analyze']:
+        st.session_state['emotion_count_dict'] = EMOTION_COUNT
+    
+    emotions = list(EMOTION_COUNT.keys())
+    counts = list(EMOTION_COUNT.values())
+    data = {'Emotion': emotions, 'Count': counts}
+    df = pd.DataFrame(data)
+    fig = px.bar(df, x='Emotion', y='Count', color='Emotion')
+    fig.update_layout(title='Emotion count')
     placeholder.write(fig)
 
 
 def make_linegraph(placeholder):
+    if st.session_state['analyze']:
+        st.session_state['emotion_dinamics'] = LINE_DATA
+
     fig = px.line(LINE_DATA)
-    fig.update_layout(xaxis_title="frame", yaxis_title="emotion", title='Emotion graph')
+    fig.update_layout(xaxis_title="Frame", yaxis_title="Emotion", title='Emotion dinamics')
     placeholder.write(fig)
 
 
 st.write("Шаг 1. Включите камеру, нажав на красную кнопку START")
 webrtc_streamer(key="example",
+                mode=WebRtcMode.SENDRECV,
                 video_frame_callback=callback,
                 media_stream_constraints={"video": True, "audio": False},
                 async_processing=True)
@@ -99,8 +122,24 @@ stop_flag = True
 
 if start_button.button('ANALYZE EMOTION', key='start'):
     start_button.empty()
+    st.session_state['analyze'] = True
     if not st.button('Stop', key='stop'):
         while True:
             make_barchart(placeholder_1)
             make_linegraph(placeholder_2)
             time.sleep(0.1)
+    else:
+        emotion_count_data = st.session_state['emotion_count_dict']
+        emotions = list(emotion_count_data.keys())
+        counts = list(emotion_count_data.values())
+        data_dict = {'Emotion': emotions, 'Count': counts}
+        df = pd.DataFrame(data_dict)
+        fig_bar = px.bar(df, x='Emotion', y='Count', color='Emotion')
+        fig_bar.update_layout(title='Emotion count')
+        st.plotly_chart(fig_bar, use_container_width=True)
+
+        emotion_dinamics_data = st.session_state['emotion_dinamics']
+        fig_line = px.line(emotion_dinamics_data)
+        fig_line.update_layout(xaxis_title="Frame", yaxis_title="Emotion", title='Emotion dinamics')
+        st.plotly_chart(fig_line, use_container_width=True)
+
